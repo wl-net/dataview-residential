d318f85f4748a2acf99a3ae1f7de7b4ed8707a7b
Andrew Sorensen <andrew@andrewsorensen.net>
2015-04-08 21:08:57 -0700

    Add models from dataview

--- residential/admin.py
+++ residential/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from residential.models import Residence, Neighbor
 
-# Register your models here.
+class ResidenceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location')
+    pass
+
+admin.site.register(Residence, ResidenceAdmin)
+
+class NeighborAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'location')
+    pass
+
+admin.site.register(Neighbor, NeighborAdmin)
--- residential/models.py
+++ residential/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Residence(UUIDModel):
+    name = models.CharField(max_length=64)
+    location = models.ForeignKey('building.Building')
+    unit_number = models.CharField(max_length=8)
+    floor = models.IntegerField(default=1)
+    rent = models.FloatField(default=0)
+    tenants = models.ManyToManyField('auth.User', blank=True)
+
+    def __unicode__(self):
+        return self.name + " " + self.location.city
+
+    def __str__(self):
+        return self.name + " " + self.location.city
+
+class Neighbor(UUIDModel):
+    user = models.ForeignKey('auth.User', editable=False)
+
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+    #location = models.ForeignKey('building.Building')
+
+    def __unicode__(self):
+        return self.first_name + " " + self.last_name
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
